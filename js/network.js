398867325dcfdea78ed295009acd7e246af61108
tkekan <tanvir.kekan@gmail.com>
2014-04-23 17:42:50 -0700

    event handler and detail table update

--- js/network.js
+++ js/network.js
@@ -1,4 +1,4 @@
-var List = [];
+List = [];
 
 var scan_function = function() {
     console.log("In scan function");
@@ -38,6 +38,15 @@ var progress = setInterval(function () {
 }, 800);
 
 
+function hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+
 var createTable = function (fullData, callback){
     var request = $.getJSON('event_log.json', function(data) {
             $.each(data, function(index, value) {
@@ -71,12 +80,69 @@ var createTable = function (fullData, callback){
                         ]
                     }); 
 
-         $("#example tbody tr").on("click",function(event) {
+         $("#example tbody").on('click','tr',function(event) {
                  packetLog = oTable.fnGetData(this);
+                 var fullLog = fullData[packetLog[0]];
                  console.log("In click event");
-                 var tableid = document.getElementById('detail_table');
-                 console.log(tableid.rows);
-            });
+                 document.getElementById('src_ip').innerHTML = packetLog[3];
+                 document.getElementById('dst_ip').innerHTML = packetLog[5];
+                 document.getElementById('ip_ver').innerHTML = fullLog.ip_ver;
+                 document.getElementById('ip_ver').innerHTML = fullLog.ip_ver;
+                 document.getElementById('ip_hl').innerHTML = fullLog.ip_hlen;
+                 document.getElementById('ip_tos').innerHTML = fullLog.ip_tos;
+                 document.getElementById('ip_tos').innerHTML = fullLog.ip_tos;
+                 document.getElementById('ip_len').innerHTML = fullLog.ip_tos;
+                 document.getElementById('ip_id').innerHTML = fullLog.ip_id;
+                 document.getElementById('ip_flags').innerHTML = fullLog.ip_flags;
+
+                 document.getElementById('ip_off').innerHTML = fullLog.ip_off;
+                 document.getElementById('ip_ttl').innerHTML = fullLog.ip_ttl;
+                 document.getElementById('ip_csum').innerHTML = fullLog.ip_csum;
+                 if( fullLog.ip_proto == 1) {
+                 //ICMP
+
+                 }
+                 else if( fullLog.ip_proto == 6) {
+                 //Currently just show for TCP
+                    document.getElementById('tcp_sport').innerHTML = fullLog.tcp_sport;
+                    document.getElementById('tcp_dport').innerHTML = fullLog.tcp_dport;
+                    document.getElementById('tcp_seq').innerHTML = fullLog.tcp_seq;
+                    document.getElementById('tcp_ackn').innerHTML = fullLog.tcp_ack;
+                    document.getElementById('tcp_off').innerHTML = fullLog.tcp_off;
+                    document.getElementById('tcp_res').innerHTML = fullLog.tcp_res;
+                    document.getElementById('tcp_win').innerHTML = fullLog.tcp_win;
+                    document.getElementById('tcp_urp').innerHTML = fullLog.tcp_urp;
+                    document.getElementById('tcp_csum').innerHTML = fullLog.tcp_csum;
+                    if(fullLog.data_payload != null) {
+                        document.getElementById('packet_data').innerHTML = fullLog.data_payload;
+                        document.getElementById('ascii_data').innerHTML = hex2a(fullLog.data_payload);
+
+                    }
+
+                    var flags = parseInt(fullLog.tcp_flags);
+                    console.log("flags in dec: " + flags);
+                    console.log("flags in log: " + fullLog.tcp_flags);
+                    var shifter_iterator = 0;
+                    var mask = 1;
+                    var tag = { 0:'tcp_fin',1:'tcp_syn',2:'tcp_rst',3:'tcp_psh',4:'tcp_ack',5:'tcp_urg',6:'tcp_r0',7:'tcp_r1'};
+                    while( shifter_iterator < 8) {
+                        if(flags & (mask << shifter_iterator)) 
+                        document.getElementById(tag[shifter_iterator]).innerHTML = 1;
+                        else
+                        document.getElementById(tag[shifter_iterator]).innerHTML = 0;
+                        shifter_iterator++;
+
+
+                    }
+
+                     
+                 }
+                 else if( fullLog.ip_proto == 17) {
+                 //UDP
+                 }
+
+
+         });
 
     });
 
